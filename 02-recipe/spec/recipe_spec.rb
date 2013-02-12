464c5711f86885bd368ff1a52250a10471cb5c91
jaketrent <trent.jake@gmail.com>
2013-02-12 10:08:59 -0700

    refactor tests

--- 02-recipe/spec/recipe_spec.rb
+++ 02-recipe/spec/recipe_spec.rb
@@ -4,15 +4,12 @@ require "recipe"
 describe Recipe do 
 
   describe "#initialize" do
-
-    before do
-      @title = "Test title"
-      @ingredients = ["Test ingredient 1"]
-      @instructions = ["Test instruction 1"]
-    end
-
+    
+    let(:title) { "Test title" }
+    let(:ingredients) { ["Test ingredient 1"] }
+    let(:instructions) { ["Test instruction 1"] }
     let(:recipe) do
-      Recipe.new @title, @ingredients, @instructions
+      Recipe.new title, ingredients, instructions
     end
 
     it "should take title, ingredients, and instructions, no optional params" do
@@ -20,22 +17,22 @@ describe Recipe do
     end
 
     it "should store the title" do
-      recipe.title.should eq @title
+      recipe.title.should eq title
     end
 
     it "should store the ingredients" do
-      recipe.ingredients.should eq @ingredients
+      recipe.ingredients.should eq ingredients
     end
 
     it "should store the instructions" do
-      recipe.instructions.should eq @instructions
+      recipe.instructions.should eq instructions
     end
 
     describe "optionaly allows servings" do
 
       let(:servings) { "Test num servings" }
       let(:recipe) do
-        recipe = Recipe.new @title, @ingredients, @instructions, servings
+        recipe = Recipe.new title, ingredients, instructions, servings
       end
       
       it "should store a serving" do
