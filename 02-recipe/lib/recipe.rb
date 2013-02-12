ff41c52fefed2c5040d55adde7b5dd366ec0f54a
jaketrent <trent.jake@gmail.com>
2013-02-12 10:59:59 -0700

    refactor out initializer params

--- 02-recipe/lib/recipe.rb
+++ 02-recipe/lib/recipe.rb
@@ -1,6 +1,10 @@
 class Recipe
 
   def initialize params, ingredients=nil, instructions=nil, servings=nil
+    parse_params params, ingredients, instructions, servings
+  end
+
+  def parse_params params, ingredients, instructions, servings
     if params.is_a? Hash then
       params.each do |k,v|
         instance_variable_set("@#{k}", v) unless v.nil?
