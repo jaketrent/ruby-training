7411112a09387b362604a9705eb4391e61bb8820
jaketrent <trent.jake@gmail.com>
2013-02-12 10:21:15 -0700

    allow listed or hashed params

--- 02-recipe/lib/recipe.rb
+++ 02-recipe/lib/recipe.rb
@@ -1,10 +1,17 @@
 class Recipe
 
-  def initialize title, ingredients, instructions, servings=nil
-    @title = title
-    @ingredients = ingredients
-    @instructions = instructions
-    @servings = servings
+  def initialize params, ingredients=nil, instructions=nil, servings=nil
+    if params.is_a? Hash then
+      @title = params[:title]
+      @ingredients = params[:ingredients]
+      @instructions = params[:instructions]
+      @servings = params[:servings]
+    else
+      @title = params
+      @ingredients = ingredients
+      @instructions = instructions
+      @servings = servings
+    end
   end
 
   attr_reader :title, :ingredients, :instructions, :servings
--- 02-recipe/spec/recipe_spec.rb
+++ 02-recipe/spec/recipe_spec.rb
@@ -4,10 +4,11 @@ require "recipe"
 describe Recipe do 
 
   describe "#initialize" do
-    
+
     let(:title) { "Test title" }
     let(:ingredients) { ["Test ingredient 1"] }
     let(:instructions) { ["Test instruction 1"] }
+    let(:servings) { "Test num servings" }
     let(:recipe) do
       Recipe.new title, ingredients, instructions
     end
@@ -30,12 +31,38 @@ describe Recipe do
 
     describe "optionaly allows servings" do
 
-      let(:servings) { "Test num servings" }
       let(:recipe) do
         recipe = Recipe.new title, ingredients, instructions, servings
       end
       
       it "should store a serving" do
+        recipe.servings.should eq servings
+      end
+
+    end
+
+    describe "named parameters" do
+
+      let(:recipe) do
+        Recipe.new title: title, 
+          ingredients: ingredients, 
+          instructions: instructions,
+          servings: servings
+      end
+
+      it "should store the named param title" do
+        recipe.title.should eq title
+      end
+
+      it "should store the named param ingredients" do
+        recipe.ingredients.should eq ingredients
+      end
+
+      it "should store the named param instructions" do
+        recipe.instructions.should eq instructions
+      end
+
+      it "should store the named param servings" do
         recipe.servings.should eq servings
       end
 
