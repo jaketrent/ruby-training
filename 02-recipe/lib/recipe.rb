df3e91b073c771adb0bd4638cee59d5d1e88cc5e
jaketrent <trent.jake@gmail.com>
2013-02-12 11:06:56 -0700

    refactor parse params to remove a condition

--- 02-recipe/lib/recipe.rb
+++ 02-recipe/lib/recipe.rb
@@ -1,20 +1,20 @@
 class Recipe
 
   def initialize params, ingredients=nil, instructions=nil, servings=nil
-    parse_params params, ingredients, instructions, servings
+    params = parse_params params, ingredients, instructions, servings
+    
+    params.each do |k,v|
+      instance_variable_set("@#{k}", v) unless v.nil?
+    end
   end
 
-  def parse_params params, ingredients, instructions, servings
-    if params.is_a? Hash then
-      params.each do |k,v|
-        instance_variable_set("@#{k}", v) unless v.nil?
-      end
-    else
-      @title = params
-      @ingredients = ingredients
-      @instructions = instructions
-      @servings = servings
-    end
+  def parse_params name_or_params, ingredients, instructions, servings
+    return name_or_params if name_or_params.is_a? Hash
+
+    { title: name_or_params,
+      ingredients: ingredients,
+      instructions: instructions,
+      servings: servings }
   end
 
   attr_reader :title, :ingredients, :instructions, :servings
