a70e93ab2f677f1f49212dfdaa83b2f27afb6e37
jaketrent <trent.jake@gmail.com>
2013-02-14 15:36:31 -0700

    order art comments

--- 04-blogger/app/models/article.rb
+++ 04-blogger/app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ActiveRecord::Base
   # limit the mass assignment thru Article.create
   attr_accessible :title, :body
   has_many :comments
+
+  def ordered_comments
+    comments.order("updated_at desc") if comments
+  end
 end
